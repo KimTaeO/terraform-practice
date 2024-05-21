5c34e685df08e4b02f6615c7c7c4a0cd1eab98c7
KimTaeO <taeo0328@gmail.com>
2024-05-21 23:55:51 +0900

    init

---
+++ 03.start/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+resource "local_file" "abc" {
+  content  = "abc!"
+  filename = "${path.module}/abc.txt"
+}
+
+resource "local_file" "def" {
+  content  = "def!"
+  filename = "${path.module}/def.txt"
+}
